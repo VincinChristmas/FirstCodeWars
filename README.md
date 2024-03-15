6191cddd5e32ab4b7e7ce005a43f84a0deea05ee
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-15 12:17:19 +0800

    added a link to the README file and uploaded a new kata today.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FirstCodeWars
 a file containing all of my codeWars kata. Maximum of 100 katas, some of which will have various solutions that I have thought of
+
+my actual CodeWars profile is linked below.
+
+[text](https://www.codewars.com/users/Vincin%20Christmas)
--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+//NUMBER 17!!!
+
+/*
+DESCRIPTION:
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
+
+
+*/
+
+//MY SOULUTION
+function isVow(a){
+  for (let i=0; i <=a.length; i++) {
+    if (a[i] == 101) {
+      a[i] = 'e'
+    } if (a[i] == 105) {
+      a[i] =  'i'
+    } if (a[i] == 111) {
+      a[i] = 'o'
+    } if (a[i] == 117) {
+      a[i] = 'u'
+    } if (a[i] == 97) {
+      a[i] = 'a'
+    }
+    
+  }
+  return a
+  }
+
 //NUMBER 16 !!!
 
 /*
