dcdf166b83b28d71533dd95fcfef1bb5d75d9c3d
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-16 22:54:55 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+//NUMBER 18!!!
+
+/*
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. 
+For calculating the average point you may add your point to the given array!
+
+
+*/
+
+//MY SOULUTION
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  let avg  = 0 
+  let newSum = classPoints.length + 1
+  classPoints.forEach((point) => (avg += point ))
+  if ((avg/newSum) > yourPoints) {
+    return false
+  } else if ((avg/newSum) < yourPoints) {
+    return true
+  }
+  
+  
+}
+
+
 //NUMBER 17!!!
 
 /*
