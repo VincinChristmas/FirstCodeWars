f55d542ef7781e6f1db702af9f07b30d46282e6a
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-27 20:50:30 +0800

    solved a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//NUMBER 51!!!
+
+/*
+Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+Some cases:
+[22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+*/
+
+//MY SOLUTION
+function multipleOfIndex(array) {
+  // good luck
+  let filterByIndex = []
+  if(array[0] === 0) 
+    filterByIndex.push(0)
+  for(let i = 1; i < array.length; i++) {
+     if (array[i] % i === 0) {
+       filterByIndex.push(array[i])
+     }
+    
+}
+  return filterByIndex
+  }
 //NUMBER 50!!!
 
 /*
