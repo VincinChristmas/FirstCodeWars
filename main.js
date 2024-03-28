b4c512e6776a4e0c171e96411d32ea9225403978
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-28 16:10:09 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//NUMBER 52!!!
+
+/*
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+*/
+
+//MY SOLUTION
+function invert(array) {
+  const invertedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    invertedArray.push(-array[i]);
+  }
+  return invertedArray;
+}
+
 //NUMBER 51!!!
 
 /*
