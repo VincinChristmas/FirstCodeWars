2c3a63b088104e7864b79edc0e9ee5d23e42ee0f
VincinChristmas <VincinChristmas@hotmail.com>
2024-03-29 12:07:50 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//NUMBER 53!!!
+
+/*
+I'm new to coding and now I want to get the sum of two arrays... 
+Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+//MY SOLUTION
+function arrayPlusArray(arr1, arr2) {
+  return (arr1.reduce((acc, curr) => acc + curr) + arr2.reduce((acc, curr) => acc + curr)); 
+}
 //NUMBER 52!!!
 
 /*
