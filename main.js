e5692a1c418b92efc5f27ec0379a468c8ede86f2
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-01 12:40:10 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+//NUMBER 55!!!
+
+/*
+SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes
+Objective
+Preloaded for you is a class Dog:
+
+class Dog {
+  constructor(name, age, gender, species, size, master, loyal) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = species;
+    this.legs = 4;
+    this.size = size;
+    this.master = master;
+    this.loyal = loyal;
+  }
+}
+You are then given a working class Labrador as your initial code.
+
+class Labrador {
+  constructor(name, age, gender, master) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = master;
+    this.loyal = true;
+  }
+}
+Shorten it so that it meets the strict character count requirements for this Kata.
+*/
+
+//MY SOLUTION
+class Labrador {
+  constructor(n, a, g, m) {
+    this.name = n;
+    this.age = a;
+    this.gender = g;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = m;
+    this.loyal = true;
+  }
+}
+
+
 //NUMBER 54!!!
 
 /*
