77d61912a0dee9f3a8eeebd86f32fc65c24c4e89
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-11 21:34:35 +0800

    added a new string kata

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//NUMBER 61!!!
+
+/*
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, 
+the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+//MY SOLUTION
+function longest(s1, s2) {
+  // your code
+  let prepedString =(s1 + s2).split('').filter((c, i, self) => {
+ return self.indexOf(c) === i;
+  })
+  return prepedString.sort().join('')
+}
+
+
 //NUMBER 60!!!
 
 /*
