59e1d57795484a5a5e8be4debd1374d12e05812f
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-18 17:10:08 +0800

    completed an object oriented programming kata today

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+//NUMBER 66!!!
+
+/*
+Our counter prototype is broken. Can you spot, what's wrong here?
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase() {
+  this.value++;
+};
+
+Counter.prototype.getValue() {
+  return this.value;
+};
+
+Counter.prototype.reset() {
+  this.value = 0;
+};
+*/
+
+//MY SOLUTION
+function Counter(value) {
+  this.value = 0;
+
+
+this.increase = function() {
+  this.value++;
+};
+
+this.getValue = function() {
+  return this.value;
+};
+
+this.reset = function() {
+  this.value = 0;
+};
+  }
+
+
 //NUMBER 65!!!
 
 /*
