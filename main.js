0474149140a8608dd098a926d98f432bb34c36d7
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-23 10:14:36 +0800

    added an array kata today

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+//NUMBER 69!!!
+
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+*/
+
+//MY SOLUTION
+function grow(x){
+  let keepMultiplying = 1
+  for (let i = 0; i < x.length; i++) {
+    keepMultiplying *= x[i]
+  }
+    return keepMultiplying
+  }
+
 //NUMBER 68!!!
 
 /*
