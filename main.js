b250a598c0e630b9c85e40be5f31de45dc27d455
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-27 17:11:36 +0800

    added another array kata that I solved that allowed me to use includes method AGAIN

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//NUMBER 75!!!
+
+/*
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+*/
+
+//MY SOLUTION
+function distinct(a) {
+  let nonDuplicates = []
+  for (let i = 0; i < a.length; i++) {
+    if (nonDuplicates.includes(a[i]) === false) {
+      nonDuplicates.push(a[i])
+    }
+  }
+  return nonDuplicates
+}
+
 //NUMBER 74!!!
 
 /*
