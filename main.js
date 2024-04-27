43765b4429a39161eb0b4134957dd1f8b7eac11c
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-27 19:07:20 +0800

    just uploaded a new array kata today that cubes and squares elements

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+//NUMBER 78!!!
+
+/*
+SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program that 
+returns true if the sum of the squares of each element in a is strictly greater than the sum of 
+the cubes of each element in b.
+
+E.g.
+
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+*/
+
+//MY SOLUTION
+function arrayMadness(a, b) {
+  
+  let sumA = 0
+  for (let i = 0; i < a.length; i++) {
+    sumA = sumA + Math.pow(a[i], 2)
+  } 
+
+  let sumB = 0
+  for (let i = 0; i < b.length; i++) {
+    sumB = sumB + Math.pow(b[i], 3)
+  }
+  return  sumA > sumB 
+
+}
+
 //NUMBER 76!!!
 
 /*
