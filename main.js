0db5ebec16b924c49b7a37afbabf2553399e70d8
VincinChristmas <VincinChristmas@hotmail.com>
2024-04-30 10:19:53 +0800

    completed another kata

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//NUMBER 80!!!
+
+/*
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family 
+member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a 
+baby of 5 months, will have an ascribed ‘age’ of 0. Return a
+ new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+*/
+
+//MY SOLUTION
+function differenceInAges(ages){
+  
+  function compareNumbers(a,b) {
+    return a-b
+  }
+
+  let sortedOut = ages.sort(compareNumbers) 
+  let youngest = sortedOut[0]
+  let oldest = sortedOut[sortedOut.length-1]
+  let theDifference = oldest - youngest
+  return [youngest, oldest, theDifference]
+}
+
 //NUMBER 79!!!
 
 /*
