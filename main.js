1ca6acb48bc7481f53a4a40593458a5887da1d41
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-01 11:13:55 +0800

    added a new array kata

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+//NUMBER 83!!!
+
+/*
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) 
+\will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+//MY SOLUTION
+function first(arr, n) {
+  let sequence = [] 
+  if (n == 0) {
+    return sequence
+  }
+  else if(typeof n==='undefined') {
+  sequence.push(arr[0])
+  return sequence
+  }
+  else {
+    for (let i = 0; i < arr.length && i < n; i++) {
+      sequence.push(arr[i])
+    }
+    return sequence
+  }
+}
+
 //NUMBER 82!!!
 
 /*
