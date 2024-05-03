5a9abf2077b0593301252a6b1ce0a51ec29c7dd8
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-03 13:26:22 +0800

    added a new basic kata

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//NUMBER 87!!!
+
+/*
+The function pickIt accepts 1 parameter, arr, which is an array of numbers. We need to traverse arr 
+by using a for loop. If an element is an odd number, push it to the odd array, and if it's an even number, push it to the evenarray.
+
+I've defined two arrays odd and even in the function, and also wrote the return statement. Your work is to write a for loop.
+*/
+
+//MY SOLUTION
+function pickIt(arr){
+  var odd=[],even=[];
+  //coding here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]%2!==0) {
+      odd.push(arr[i])
+    }
+     else if(arr[i]%2===0) {
+      even.push(arr[i])
+    }
+    
+  }
+  
+  
+  return [odd,even];
+}
+
 //NUMBER 86!!!
 
 /*
