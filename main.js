a6dc66a221c3ff6bbe5a8b4c55c4cf098a6c501a
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-08 06:00:24 +0800

    attemted a while loop kata today

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+//NUMBER 94!!!
+
+/*
+Complete function padIt, which accepts 2 parameters:
+
+str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+n: a number indicating how many times to pad the string.
+Behaviour
+You need to write a loop statement within the function that loops n times. Each time through the 
+loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the 
+left side of str, the second time will add a * to the right side, and so on.
+
+Finally, return the padded string.
+*/
+
+//MY SOLUTION
+function padIt(str,n){
+
+  let counter = 1
+  while (counter <= n) {
+    
+    if (counter % 2 !== 0) {
+         str = '*' + str
+    }
+    if (counter % 2 == 0) {
+      str = str + '*'
+      }
+    counter++
+  }
+  return str
+}
+
+
+
 //NUMBER 93!!!
 
 /*
