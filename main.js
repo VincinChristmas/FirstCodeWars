0390e3203395f16a1cbcf56bb695c7a90188bf2a
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-12 10:14:05 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+//NUMBER 99 !!!
+
+/*
+
+*/
+
+//MY SOLUTION
+
+
+//NUMBER 98 !!!
+
+/*
+Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+
+Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+
+the color code should starting with "#". and then use 2 characters per color.
+
+for example:
+
+colorOf(255,0,0) should return "#ff0000"
+colorOf(0,111,0) should return "#006f00"
+colorOf(1, 2 ,3) should return "#010203"
+That's all of your work. My work is print your color code on your screen.
+*/
+
+//MY SOLUTION
+function colorOf(r,g,b){
+  //coding here
+  let first = r.toString(16);
+  let second = g.toString(16);
+  let third = b.toString(16);
+  
+  if (r < 16) {
+    first = `${0}${r.toString(16)}`
+  }
+  if (g < 16 ) {
+    second = `${0}${g.toString(16)}`
+  }
+  if (b < 16) {
+    third = `${0}${b.toString(16)}`
+  }
+  
+  return `#${first}${second}${third}`
+  
+}
+
 //NUMBER 97 !!!
 
 /*
