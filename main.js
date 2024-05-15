8babff3a8765d7b64178299a9f0fa50dedaf5c65
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-15 12:38:46 +0800

    added the 100th kata today ! marking the end of this repository

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+//100th !!!
+
+/*
+Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+The first mission: Traversing arr, find the shortest string length.
+
+The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). 
+you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+for example:
+
+cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+cutIt(["codewars","javascript","java"]) should return ["code","java","java"]
+*/
+
+//MY SOLUTION
+function cutIt(arr){
+  function compareNumbers(a, b) {
+  return a - b;
+}
+  
+let captureLength = []
+let finalCapture = []
+  for (let i = 0; i < arr.length; i++) {
+    captureLength.push(arr[i].length)
+    captureLength = captureLength.sort(compareNumbers)
+  }
+  for (let i = 0; i < arr.length; i++) {
+    finalCapture.push(arr[i].substr(0, captureLength[0]))
+  }
+  return finalCapture
+}
+
 //NUMBER 99 !!!
 
 /*
